cd263660e0696cfd61d514da2674efc2ed2fb298
Juvenal Campos <ctjuvenal@gmail.com>
2023-05-31 14:16:36 -0600

    small correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
-  root to: 'application#home'
+  root to: 'restaurants#index'
 end
